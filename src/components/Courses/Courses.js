fb9d2a35ab70f5c072f6f38424a128e4ed036c54
Raiyan109 <kraiyan109@gmail.com>
2022-08-20 23:08:28 +0600

    live deploy

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Course from './Course';
 import styles from './Courses.module.css'
 const Courses = () => {
 
-    const courses = [
-        {
-            id: 1,
-            "name": "React Course",
-            "img": "../../assets/images/course-1.png",
-            "instructor": "Raiyan Kabir",
-            "price": "14.34",
-        },
-        {
-            id: 2,
-            "name": "HTML CSS Course",
-            "img": "../../assets/images/course-2.png",
-            "instructor": "Raiyan Kabir",
-            "price": "14.34",
-        },
-        {
-            id: 3,
-            "name": "Javascript Course",
-            "img": "../../assets/images/course-3.png",
-            "instructor": "Raiyan Kabir",
-            "price": "14.34",
-        },
-        {
-            id: 4,
-            "name": "Node JS Course",
-            "img": "../../assets/images/course-3.png",
-            "instructor": "Raiyan Kabir",
-            "price": "14.34",
-        },
+    const [courses, setCourses] = useState([])
 
-    ]
+    useEffect(() => {
+        fetch('courses.json')
+            .then(res => res.json())
+            .then(data => setCourses(data))
+    }, [])
+    // const courses = [
+    //     {
+    //         id: 1,
+    //         "name": "React Course",
+    //         "img": "../../assets/images/course-1.png",
+    //         "instructor": "Raiyan Kabir",
+    //         "price": "14.34",
+    //     },
+    //     {
+    //         id: 2,
+    //         "name": "HTML CSS Course",
+    //         "img": "../../assets/images/course-2.png",
+    //         "instructor": "Raiyan Kabir",
+    //         "price": "14.34",
+    //     },
+    //     {
+    //         id: 3,
+    //         "name": "Javascript Course",
+    //         "img": "../../assets/images/course-3.png",
+    //         "instructor": "Raiyan Kabir",
+    //         "price": "14.34",
+    //     },
+    //     {
+    //         id: 4,
+    //         "name": "Node JS Course",
+    //         "img": "../../assets/images/course-3.png",
+    //         "instructor": "Raiyan Kabir",
+    //         "price": "14.34",
+    //     },
+
+    // ]
     return (
         <div className={styles.container}>
             {
